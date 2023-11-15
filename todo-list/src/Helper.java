f7e82629bf4d05d7a9de3065564143c621afe330
hongwang <hongwang@localhost>
2023-11-14 22:25:44 -0800

    update helper

--- todo-list/src/Helper.java
+++ todo-list/src/Helper.java
@@ -1,15 +1,8 @@
 import java.util.List;
 
 public class Helper {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
-	
-	
-	// TODO
+	// Function Implementations
 	public List<Task> SortTasks(List<Task> input){
 		return null;
 		
