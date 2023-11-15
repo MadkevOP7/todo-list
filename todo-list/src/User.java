2966d33f5de586cd50f2dd1fe35180c6b11f1dac
hongwang <hongwang@localhost>
2023-11-14 21:12:15 -0800

    completed user data member

--- todo-list/src/User.java
+++ todo-list/src/User.java
@@ -1,8 +1,11 @@
+import java.util.List;
+import java.util.Locale.Category;
 
 public class User {
-	String usernameString;
-	String passworString;
-	String hi;
-	String bye;
-	String hongString;
+	public String username;
+	public String password;
+	public String email;
+	public String f_name;
+	public String l_name;
+	public List<Category> categories;
 }
