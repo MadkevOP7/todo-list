8e519d12449d13d1432b31df299a55dd5b08ba56
hongwang <hongwang@192.168.0.228>
2023-11-19 20:07:10 -0800

    added firebase jar

--- todo-list/src/Category.java
+++ todo-list/src/Category.java
@@ -3,3 +3,13 @@ import java.util.List;
 public class Category {
 	public List<TList> lists;
 }
+
+
+user
+	categories	
+		list1
+			tlist1
+				task 1
+				task2
+			tlist2
+		list2
--- todo-list/src/LoginServlet.java
+++ todo-list/src/LoginServlet.java
@@ -15,17 +15,17 @@ public class LoginServlet extends HttpServlet{
 		
 	private static final long serialVersionUID = 1L;
 
-		//NEED TO FINISH
+	//NEED TO FINISH
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		PrintWriter pw = response.getWriter();
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		
-		String username = request.getParameter("username");
+		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		Gson gson = new Gson();
 		
-		if (username == null || username.isBlank() || password == null || password.isBlank()) {
+		if (email == null || email.isBlank() || password == null || password.isBlank()) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			String error = "Info missing";
 			pw.write(gson.toJson(error));
@@ -33,11 +33,10 @@ public class LoginServlet extends HttpServlet{
 		}
 		
 		try {
-			if (JDBC_Connector.validate_login(username, password)) {//login info is valid, redirect logged in user
+			if (Helper.validate_user(email, password)) {//login info is valid, redirect logged in user
 				//add the logged in user cookie
-				Cookie c = new Cookie(username, "LoggedIn");
+				Cookie c = new Cookie(email, "LoggedIn");
 				response.addCookie(c);
-			    response.getWriter().write(username);
 				response.sendRedirect("home.html");
 			}
 			else {//login credential is incorrect
@@ -46,9 +45,6 @@ public class LoginServlet extends HttpServlet{
 				pw.write(gson.toJson(error));
 				pw.flush();
 			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("no class found");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
