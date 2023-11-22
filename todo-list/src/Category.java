a9dea68760f3cda3e983c910c54377e7e8048657
Kevin Yang <68880349+MadkevOP7@users.noreply.github.com>
2023-11-21 18:35:34 -0800

    fix all errors

--- todo-list/src/Category.java
+++ todo-list/src/Category.java
@@ -1,15 +1,3 @@
-import java.util.List;
 
 public class Category {
-	public List<TList> lists;
 }
-
-
-user
-	categories	
-		list1
-			tlist1
-				task 1
-				task2
-			tlist2
-		list2
--- todo-list/src/Helper.java
+++ todo-list/src/Helper.java
@@ -2,11 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 public class Helper {
 	
@@ -26,22 +22,13 @@ public class Helper {
 	
 	public static void main(String[] args) {
 		
-		// Use the application default credentials
-		GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-		FirebaseOptions options = new FirebaseOptions.Builder()
-		    .setCredentials(credentials)
-		    .setProjectId(projectId)
-		    .build();
-		FirebaseApp.initializeApp(options);
-
-		Firestore db = FirestoreClient.getFirestore();
+		
 	}
 
 }
 
 class DueDateSort implements Comparator<Task>{
 
-	@Override
 	public int compare(Task t1, Task t2) {
 		
 		if(t1.dueYear > t2.dueYear) return -1; 
